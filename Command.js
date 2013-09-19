7adf42155a90b5fd286c873d3be0810575008a4f
Jake Scott <jake.y.scott@gmail.com>
2013-09-19 15:46:57 -0400

    Update Command.js
    Added ability to buffer multiple commands into a single action

--- Command.js
+++ Command.js
@@ -2,20 +2,27 @@
 	var doChain = [];
 	var undoChain = [];
 	var maxLength = 100;
+	var buffer = [];
+	var buffering = false;
 
 	var Command = {	
 		do: function(up, down) {
 			if(!up) {
-				throw "UndoRedo.do missing required parameter 'up'."
+				throw "Command.do missing required parameter 'up'."
 			}
 			else if(!down) {
-				throw "UndoRedo.do missing required parameter 'down'."
+				throw "Command.do missing required parameter 'down'."
 			}
 			else {
+
 				var action = {
 					up: up,
 					down: down
 				};
+
+				if(buffering) {
+					buffer[buffer.length] = action;
+				}
 				undoChain = [];
 				action.up();
 				doChain.push(action);
@@ -23,9 +30,13 @@
 					doChain.shift();
 				}
 			}
-		};
+		},
 
 		undo: function() {
+			if(buffering) {
+				this.clearBuffer();
+			}
+
 			var undone = doChain.pop();
 			if(undone) {
 				undone.down();
@@ -34,9 +45,13 @@
 			else {
 				console.warn("Nothing to undo.");
 			}
-		};
+		},
 
 		redo: function() {
+			if(buffering) {
+				this.clearBuffer();
+			}
+
 			var redone = undoChain.pop();
 			if(redone) {
 				redone.up();
@@ -45,6 +60,47 @@
 			else {
 				console.warn("Nothing to redo");
 			}
+		},
+
+		bufferActions: function() {
+			buffering = true;
+		},
+
+		stopBuffer: function() {
+			if(!buffering) return;
+
+			buffering = false;
+			var stored = buffer.slice();
+			buffer = [];
+			
+			var n = stored.length;
+			for(var i=0; i<n; i++) {
+				doChain.pop();
+			}
+
+			var action = {
+				up: function() {
+					for(var i=0; i<n; i++) {
+						stored[i].up();
+					}
+				}, 
+				down: function() {
+					for(var i=n-1; i>=0; i--) {
+						stored[i].down();
+					}
+				}
+			}
+
+			undoChain = [];
+			doChain.push(action);
+			while(doChain.length > maxLength) {
+				doChain.shift();
+			}
+		},
+
+		clearBuffer: function() {
+			buffer = [];
+			buffering = false;
 		}
 	};
 
